998c40873540382b9a9e7ba6c8932061701160a3
Sahil Nolia <106691600+noliaz@users.noreply.github.com>
2022-06-09 18:21:42 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
+
+if ENVIRONMENT:
+    try:
+        API_ID = int(os.environ.get('API_ID', 0))
+    except ValueError:
+        raise Exception("Your API_ID is not a valid integer.")
+    API_HASH = os.environ.get('API_HASH', None)
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
+    CHAT = os.environ.get('CHAT', None)
+    USERS = os.environ.get('USERS', None)
+    LINK_CHAT = os.environ.get('LINK_CHAT', None)
+    ZIP_CHAT = os.environ.get('ZIP_CHAT', None)
+    FILES_CHAT = os.environ.get('FILES_CHAT', None)
+    NAMES_CHAT = os.environ.get('NAMES_CHAT', None)
+    NAMES_TEXT_CHAT = os.environ.get('NAMES_TEXT_CHAT', None)
+    
+
+  
+else:
+  
+    API_ID = 0
+    API_HASH = ""
+    BOT_TOKEN = ""
+    CHAT = 0
+    USERS = ""
+
---
+++ main.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, idle
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+import alive, config
+
+
+API_ID = config.API_ID
+API_HASH = config.API_HASH
+BOT_TOKEN = config.BOT_TOKEN
+
+
+app = Client(
+    "linkforward",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="bot"),
+)
+
+
+if __name__ == "__main__":
+    try:
+        alive.alive()
+        app.start()
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH is not valid.")
+    except AccessTokenInvalid:
+        raise Exception("Your BOT_TOKEN is not valid.")
+    uname = app.get_me().username
+    print(f"⚡ @{uname} Started Successfully!")
+    idle()
+    app.stop()
+    print("Bot stopped!")
+
